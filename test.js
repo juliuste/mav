5be25301948893e71a8ac4c63c79cc449da58fc6
Julius Tens <mail@juliustens.eu>
2019-06-23 22:52:23 +0200

    lodash.package -> lodash/package

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ const tapeWithoutPromise = require('tape')
 const addPromiseSupport = require('tape-promise').default
 const tape = addPromiseSupport(tapeWithoutPromise)
 const mav = require('.')
-const isString = require('lodash.isstring')
-const isNumber = require('lodash.isnumber')
-const isDate = require('lodash.isdate')
-const isObject = require('lodash.isobject')
+const isString = require('lodash/isString')
+const isNumber = require('lodash/isNumber')
+const isDate = require('lodash/isDate')
+const isObject = require('lodash/isObject')
 const moment = require('moment-timezone')
 
 const isStation = (s) => s.type === 'station' && isString(s.id) && s.id.length >= 1
